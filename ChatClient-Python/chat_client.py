5311fb4b34a52531e23117bc6152a9d0478f85b2
Oldrej <oleandra@stud.ntnu.no>
2020-09-28 18:06:21 +0200

    Socket defintions

--- ChatClient-Python/chat_client.py
+++ ChatClient-Python/chat_client.py
@@ -16,7 +16,7 @@ states = [
 ]
 TCP_PORT = 1300  # TCP port used for communication
 SERVER_HOST = "datakomm.work"  # Set this to either hostname (domain) or IP address of the chat server
-#Using provided server as stated in documentation
+# Using provided server as stated in documentation
 
 # --------------------
 # State variables
